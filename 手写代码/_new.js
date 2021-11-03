9076c1cdaa1a75e9947bfe26b2917a0be2bfd961
maxiao <m543438924@163.com>
2021-11-03 22:36:45 +0800

    feat:修改new

--- 手写代码/_new.js
+++ 手写代码/_new.js
@@ -9,10 +9,16 @@
  */
 
 function myNew() {
+  // 创建一个空对象
   let obj = {};
-  let construct = [].shift(arguments);
+  // 获取构造函数
+  let construct = [].shift.call(arguments);
+  // 将对象的原型链指向构造函数的原型属性
   obj.__proto__ = construct.prototype;
+  // 修改构造函数的this指向
   let result = construct.apply(obj, arguments);
+  // 构造函数返回的是一个对象 ，返回
+  // 否则返回对象
   return typeof result === 'object' ? result : obj;
 
 }
