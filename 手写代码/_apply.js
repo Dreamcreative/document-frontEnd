cf988255d32acce460496484154c21032fa042c8
maxiao <m543438924@163.com>
2021-11-08 00:12:02 +0800

    feat:新增 call apply

---
+++ 手写代码/_apply.js
@@ -0,0 +1,19 @@
+/**
+ * dosomething.apply(window,[a,b,c])
+ * @param {*} context 调用apply的函数
+ * @param {array} arg 
+ * @returns 
+ */
+Function.prototype.myApply = function (context, arg) {
+  if (typeof this !== "function") throw Error('not a function');
+  context = context ?? window;
+  context.fn = this;
+  let result;
+  if (arg) {
+    result = context.fn(...arg);
+  } else {
+    result = context.fn();
+  }
+  delete context.fn;
+  return result;
+}
---
+++ 手写代码/_call.js
@@ -0,0 +1,14 @@
+/**
+ * dosomething.call(window,a,b,c)
+ * @param {*} context 调用 call 的函数
+ * @param  {...any} args 传入 call 的参数 可以是多个参数
+ * @returns args 参数的调用结果
+ */
+Function.prototype.myCall = function (context, ...args) {
+  if (typeof this !== 'function') throw Error('not a function');
+  context = context ?? window;
+  context.fn = this;
+  const result = context.fn(...args);
+  delete context.fn;
+  return result;
+}
