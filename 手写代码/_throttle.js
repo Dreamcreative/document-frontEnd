af593ca58b0bb8fb3371f0a687148b631ff1b5c0
maxiao <m543438924@163.com>
2021-11-19 00:06:48 +0800

    feat: throttle 节流

---
+++ 手写代码/_throttle.js
@@ -0,0 +1,18 @@
+// 函数节流
+// 持续触发函数，在 wait 时间内，不管触发多少次，只会触发一次函数
+/**
+ * 定时器方式
+ * @param {function} func 节流函数
+ * @param {number} wait 节流时间
+ */
+function _throttle(func, wait) {
+  let timer = null;
+  return function () {
+    if (!timer) {
+      timer = setTimeout(() => {
+        func.apply(this, arguments);
+        timer = null;
+      }, wait)
+    }
+  }
+}
