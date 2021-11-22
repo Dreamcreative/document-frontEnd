2bfd1e139680e12596c76ae455fbf63c9ee54e25
maxiao <m543438924@163.com>
2021-11-22 22:40:39 +0800

    feat:新增 _curry

---
+++ 手写代码/_curry.js
@@ -0,0 +1,18 @@
+// 函数柯里化
+// 柯里化是一种将使用多个参数的一个函数转换为一系列使用一个参数的函数
+function _curry() {
+  const fn = [].shift.call(arguments);
+  let _args = [...arguments];
+  return function temp() {
+    if (arguments.length) {
+      _args = [..._args, ...arguments];
+      return temp;
+    }
+    return fn.apply(this, _args)
+  }
+}
+function add(...args) {
+  return args.reduce((a, b) => a + b, 0)
+}
+let result = _curry(add, 5, 1)
+console.log(result(5)(1, 2, 3)())
