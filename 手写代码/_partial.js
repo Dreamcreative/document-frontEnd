0a36b46b86109bff39373e7f859737e44fa1af48
maxiao <m543438924@163.com>
2021-11-22 23:07:52 +0800

    feat:新增 _partial 偏函数

---
+++ 手写代码/_partial.js
@@ -0,0 +1,15 @@
+// 偏函数
+// 指定一个函数的一部分参数，然后产生一个更小元的函数
+// 什么是元，元是只函数参数的个数，
+
+/**
+ * 
+ */
+function _partial() {
+  const fn = [].shift.call(arguments);
+  let args = [...arguments];
+  return function () {
+    let newArgs = args.concat([...arguments]);
+    return fn.apply(this, newArgs);
+  }
+}
