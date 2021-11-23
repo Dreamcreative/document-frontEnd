1c0300f937e92e3c8325153a71f3a466661d0a3b
maxiao <m543438924@163.com>
2021-11-23 18:58:30 +0800

    feat:新增inserSort插入排序

---
+++ algorithm/排序/insertSort.js
@@ -0,0 +1,22 @@
+// 插入排序
+/**
+ * 1. 从第二个元素开始，遍历它之前的元素
+ * 2. 如果它之前的元素大于它，那么交换，并继续向前遍历之前比它大的元素
+ * @param {*} arr 
+ */
+function insertSort(arr) {
+  const len = arr.length;
+  for (let outer = 1; outer < len; outer++) {
+    let inner = outer;
+    let temp = arr[outer];
+    while (inner > 0 && arr[inner - 1] > temp) {
+      arr[inner] = arr[inner - 1];
+      inner--;
+    }
+    arr[inner] = temp;
+  }
+  return arr;
+}
+let arr = [146, 248, 31, 45, 9, 11, 145, 300];
+// [31, 146, 248, 45, 9, 11, 145, 300]
+insertSort(arr)
