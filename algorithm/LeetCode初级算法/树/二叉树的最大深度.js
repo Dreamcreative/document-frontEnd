57fb67e0f747861c7c3506f6f1898c5fe4c2e634
maxiao <m543438924@163.com>
2022-01-18 22:10:49 +0800

    feat:leetcode 二叉树的最大深度

---
+++ algorithm/LeetCode初级算法/树/二叉树的最大深度.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// [3, 9, 20, null, null, 15, 7]
+
+//      3
+//   9    20
+//       15  7
+var maxDepth = function (root) {
+  /**
+  深度优先遍历
+   */
+  // if (!root) return 0;
+  // return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
