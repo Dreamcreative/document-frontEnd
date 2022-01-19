dd0c4954c0710ba02f7f67958be794106ff9958b
maxiao <m543438924@163.com>
2022-01-19 16:34:17 +0800

    feat:Object.create(obj, propertiesObject)

---
+++ 手写代码/_create.js
@@ -0,0 +1,12 @@
+/**
+创建一个新对象，使用 obj,作为新对象的 __proto__
+var newObj = Object.create(obj [,propertiesObject])
+obj: 新创建对象的原型对象
+propertiesObject: 一个对象，作为返回新对象的自身属性，而不是原型链上的属性
+ */
+
+function _create(proto, propertiesObject) {
+  function F() { }
+  F.prototype = proto;
+  return new F();
+}
