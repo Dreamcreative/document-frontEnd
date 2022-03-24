33a45ca00a40be53c021b20846cc643716110bda
maxiao <m543438924@163.com>
2022-03-25 00:19:04 +0800

    feat:添加 _flatten2 控制扁平化深度

--- 手写代码/_flatten.js
+++ 手写代码/_flatten.js
@@ -8,3 +8,18 @@ function _flatten(arr) {
     return Array.isArray(cur) ? [...total, ...cur] : [...total, cur];
   }, [])
 }
+// 可以控制 平铺层级的 扁平化
+/**
+ * 
+ * @param {Array} arr 需要扁平的数组
+ * @param {*} depth 扁平深度
+ * @param {*} init 初始值
+ */
+function _flatten2(arr, depth = 1, init = []) {
+  return arr.reduce((total, cur) => {
+    if (Array.isArray(cur) && depth > 1) {
+      return _flatten2(cur, depth - 1, total)
+    }
+    return total.concat(cur);
+  }, init)
+}
