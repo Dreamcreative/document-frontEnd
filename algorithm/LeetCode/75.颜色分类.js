93d1fdd452d4e2b105a9d462a1f8b065dd27c228
maxiao <m543438924@163.com>
2022-04-10 23:54:24 +0800

    feat:LeetCode 75.颜色分类

--- algorithm/LeetCode/75.颜色分类.js
+++ algorithm/LeetCode/75.颜色分类.js
@@ -1,3 +1,5 @@
+// https://leetcode-cn.com/problems/sort-colors/solution/yan-se-fen-lei-by-leetcode-solution/
+
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
@@ -6,8 +8,9 @@ var sortColors = function (nums) {
   /**
    * 方法一 双指针
     nums 中只存在 0 1 2
-    遍历 nums ,将0 放在 L 指针的位置 ，
-    将 2 放在 R 指针位置
+    遍历 nums ,
+    将 0 放在 L 指针的位置 ，i L 索引位置的值进行交换 进行下一个 i 值的比较
+    将 2 放在 R 指针位置 i R 索引值进行交换，当 i R 值交换后，由于 交换后的 i 索引值还没有进行比较，所以 继续比较当前 i 位置的值
    */
   const len = nums.length;
   if (len < 2) return nums;
